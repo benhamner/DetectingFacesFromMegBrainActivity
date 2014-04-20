eea7de08fe149951e7a7cfb052906e711a4405ba
Ben Hamner <ben@benhamner.com>
2014-04-19 18:38:32 -0700

    updated summary

--- src/data_summary.jl
+++ src/data_summary.jl
@@ -2,14 +2,29 @@ using MAT
 
 data_path = ARGS[1]
 
-for train_subject=1:16
-    f = matopen(joinpath(data_path, @sprintf("train_subject%02d.mat", train_subject)))
+is_train_subject(subject) = subject <= 16
+
+function subject_file(subject)
+    if is_train_subject(subject)
+        return @sprintf("train_subject%02d.mat", subject)
+    else
+        return @sprintf("test_subject%02d.mat",  subject)
+    end
+end
+
+for subject=1:23
+    f = matopen(joinpath(data_path, subject_file(subject)))
     X = read(f, "X")
-    y = read(f, "y")
+    if is_train_subject(subject)
+        println("Train Subject ", subject)
+        y = read(f, "y")
+        println("--Number of Faces:    \t", sum(y))
+    else
+        println("Test  Subject ", subject)
+    end
     sampling_frequency = read(f, "sfreq")
     tmin = read(f, "tmin")
     samples_before = int(-tmin*sampling_frequency)
-    println("Subject ", train_subject)
     println("--Sampling Frequency: \t", sampling_frequency)
     println("--Time Before Stimuli:\t", tmin)
 end
