c945ca7eebd4ad3b1e070b25d084eb5b289a0601
Ben Hamner <ben@benhamner.com>
2014-04-27 23:53:01 -0700

    added topomaps

--- src/plot_grand_averages.jl
+++ src/plot_grand_averages.jl
@@ -7,6 +7,7 @@ using PyCall
 
 @pyimport scipy.cluster.hierarchy as hcluster
 @pyimport scipy.spatial.distance as dist
+@pyimport mne
 
 data_path   = ARGS[1]
 output_path = ensure_empty_directory_exists(ARGS[2])
@@ -15,10 +16,14 @@ grand_average_path = joinpath(output_path, "GrandAverage")
 fft_path           = joinpath(output_path, "FFT")
 channel_path       = joinpath(output_path, "Channels")
 z_score_path       = joinpath(output_path, "ZScore")
+difference_path    = joinpath(output_path, "Difference")
+topomaps_path      = joinpath(output_path, "Topomaps")
 mkdir(grand_average_path)
 mkdir(fft_path)
 mkdir(channel_path)
 mkdir(z_score_path)
+mkdir(difference_path)
+mkdir(topomaps_path)
 
 rainbow = Scale.ContinuousColorScale(Scale.lab_gradient(ColorValue[color(c) for c=["#3288bd","#99d594","#e6f598","#fee08b","#fc8d59","#d53e4f"]]...))
 
@@ -50,11 +55,13 @@ function plot_z_score(X_face, X_no_face, title)
     score = score[I,:]
     channels = repmat([1:num_channels], 1, num_time_samples) 
     time     = repmat(transpose([1:num_time_samples]), num_channels, 1)
-    df = DataFrame(ZScore=vec(score), Channels=vec(channels), Time=vec(time))
+    df = DataFrame(ZScore=vec(score), Channels=vec(channels), Time=vec(time), Difference=vec(difference))
     axes = Coord.cartesian(xmin=1, xmax=num_time_samples, ymin=1, ymax=num_channels)
     p = plot(df, x="Time", y="Channels", color="ZScore", Geom.rectbin, rainbow, axes)
     writecsv(joinpath(z_score_path, @sprintf("%s.csv", title)), score)
     draw(PNG(joinpath(z_score_path, @sprintf("%s.png", title)), 2*28cm, 2*21.6cm), p)
+    p = plot(df, x="Time", y="Difference", color="Channels", Geom.line, rainbow)
+    draw(PNG(joinpath(difference_path, @sprintf("%s.png", title)), 2*28cm, 2*21.6cm), p)
 end
 
 function plot_fft(X, sampling_rate, title)
@@ -75,11 +82,32 @@ function plot_channel_averages(face, no_face, subject_channel_path)
     end
 end
 
+function plot_topomaps(face, no_face, times, subject)
+    num_channels = size(face, 1)
+    layout = mne.layouts[:read_layout]("Vectorview-all")
+    evoked = mne.fiff[:Evoked](pyeval("None"))
+    evoked[:data]  = face-no_face
+    evoked[:times] = times
+    evoked[:info]  = {"ch_names" => layout[:names],
+                      "chs" => [{"kind"=>1,"unit"=>112} for i=1:num_channels],
+                      "nchan"=>num_channels}
+    p = mne.viz[:plot_evoked_topomap](evoked,
+                                      times=[0.1:0.1:1.0],
+                                      layout=layout,
+                                      proj=pyeval("False"),
+                                      size=3,
+                                      show=pyeval("False"))
+    p[:savefig](joinpath(topomaps_path, @sprintf("Subject%d.png", subject)))
+end
+
 for subject=train_subjects
     f = matopen(joinpath(data_path, subject_file(subject)))
     X     = read(f, "X")
     sfreq = read(f, "sfreq")
-    y = read(f, "y")
+    y     = read(f, "y")
+    tmin  = read(f, "tmin")
+    tmax  = read(f, "tmax")
+    times = linspace(tmin, tmax, size(X,3))
     plot_fft(X, sfreq, @sprintf("Subject %d-Face", subject))
     plot_z_score(X[vec(y).==1,:,:], X[vec(y).!=1,:,:], @sprintf("Subject %d", subject))
     #(b,a) = power_filter(sfreq, 50)
@@ -93,6 +121,7 @@ for subject=train_subjects
     num_time_samples = size(X,3)
     face    = reshape(mean(X[vec(y).==1,:,:], 1), num_channels, num_time_samples)
     no_face = reshape(mean(X[vec(y).!=1,:,:], 1), num_channels, num_time_samples)
+    plot_topomaps(face, no_face, times, subject)
     plot_grand_average(face, @sprintf("Subject %d-Face", subject))
     plot_grand_average(no_face, @sprintf("Subject %d-No Face", subject))
     plot_grand_average(face-no_face, @sprintf("Subject %d-Face-No Face", subject))
